73a5fede2cffc5244a61aa7dd081800132b0f343
Sandeep Armal <armalsandip@gmail.com>
2020-08-17 22:25:34 +0530

    Added empty fragment in activity

---
+++ app/src/main/java/com/jet2travel/blogpost/extensions/FragmentExtFunctions.kt
@@ -0,0 +1,18 @@
+package com.jet2travel.blogpost.extensions
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+
+fun AppCompatActivity.addFragment(fragment: Fragment, frameId: Int) {
+    supportFragmentManager.inTransaction {
+        add(frameId, fragment)
+    }
+}
+
+inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> Unit) {
+    val fragmentTransaction = beginTransaction()
+    fragmentTransaction.func()
+    fragmentTransaction.commit()
+}
---
+++ app/src/main/java/com/jet2travel/blogpost/ui/fragment/BlogFragment.kt
@@ -0,0 +1,40 @@
+package com.jet2travel.blogpost.ui.fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jet2travel.blogpost.R
+
+
+class BlogFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_blog_list, container, false)
+
+
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+}
