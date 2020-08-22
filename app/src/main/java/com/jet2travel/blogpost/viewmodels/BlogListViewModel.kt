b66e6e570faca35ea6cbd5e72f88dfa47c9a7cbf
Sandeep Armal <armalsandip@gmail.com>
2020-08-22 15:26:00 +0530

    Added view model

---
+++ app/src/main/java/com/jet2travel/blogpost/viewmodels/BlogListViewModel.kt
@@ -0,0 +1,18 @@
+package com.jet2travel.blogpost.viewmodels
+
+import android.app.Application
+import android.content.Context
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.jet2travel.blogpost.data.Blog
+import com.jet2travel.blogpost.repository.BlogRepository
+
+class BlogListViewModel(application: Application) : AndroidViewModel(application) {
+
+    // Expose LiveData Facts query so the UI can observe it.
+    fun getBlogListObservable(context: Context): LiveData<List<Blog>> {
+        //return blogListObservable
+        return BlogRepository.getInstance().getBlogList(context)
+    }
+
+}
